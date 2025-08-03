f617a7fe307725c0022118b14a379cae8c522982
Erasmo Frota <erasmofrota@gmail.com>
2025-08-03 12:26:24 -0300

    Adicionado url amigável

--- assets/js/lista-participantes.js
+++ assets/js/lista-participantes.js
@@ -26,11 +26,31 @@ jQuery(document).ready(function ($) {
     });
 
     // Quando o usuário seleciona um evento, busca automaticamente
-    $('#evento_id').on('change', function () {
-        if ($(this).val()) {
-            buscarParticipantes();
-        } else {
-            $('#resultadoListaParticipantes').empty();
-        }
-    });
+    // Quando o usuário seleciona um evento, atualiza a URL e busca automaticamente
+$('#evento_id').on('change', function () {
+    const selectedOption = $(this).find('option:selected');
+    const eventId = selectedOption.val();
+    const eventTitle = selectedOption.text();
+
+    if (eventId) {
+        // Gera slug do nome do evento
+        const slug = eventTitle
+            .toLowerCase()
+            .normalize('NFD') // remove acentos
+            .replace(/[\u0300-\u036f]/g, '') // remove combinações
+            .replace(/[^a-z0-9]+/g, '-') // troca por hífen
+            .replace(/^-+|-+$/g, ''); // limpa início/fim
+
+        // Atualiza a query string da URL sem recarregar a página
+        const params = new URLSearchParams(window.location.search);
+        params.set('evento', slug);
+        const novaUrl = `${window.location.pathname}?${params.toString()}`;
+        window.history.pushState({}, '', novaUrl);
+
+        buscarParticipantes();
+    } else {
+        $('#resultadoListaParticipantes').empty();
+    }
+});
+
 });
